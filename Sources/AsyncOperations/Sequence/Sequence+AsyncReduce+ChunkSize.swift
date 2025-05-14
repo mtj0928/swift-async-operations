a1ffb5f96a904fbdea85b10581670de8e62989e1
Taichone <taichone.dev@gmail.com>
2025-05-14 16:42:05 +0900

    wip

---
+++ Sources/AsyncOperations/Sequence/Sequence+AsyncReduce+ChunkSize.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+extension Sequence where Element: Sendable {
+    /// An async function of `reduce` with chunk size.
+    public func asyncReduce<Result: Sendable>(
+        _ initialResult: Result,
+        chunkSize: Int,
+        priority: TaskPriority? = nil,
+        _ nextPartialResult: @escaping @Sendable (Result, Element) async throws -> Result
+    ) async rethrows -> Result {
+        let array = Array(self)
+        let chunks = stride(from: 0, to: array.count, by: chunkSize).map {
+            Array(array[$0..<Swift.min($0 + chunkSize, array.count)])
+        }
+        
+        return try await withThrowingTaskGroup(of: Result.self) { group in
+            var result = initialResult
+            
+            for chunk in chunks {
+                let currentResult = result
+                group.addTask(priority: priority) { @Sendable in
+                    var chunkResult = currentResult
+                    for element in chunk {
+                        chunkResult = try await nextPartialResult(chunkResult, element)
+                    }
+                    return chunkResult
+                }
+            }
+            
+            while let chunkResult = try await group.next() {
+                result = chunkResult
+            }
+            
+            return result
+        }
+    }
+    
+    /// An async function of `reduce` with chunk size.
+    public func asyncReduce<Result: Sendable>(
+        into initialResult: Result,
+        chunkSize: Int,
+        priority: TaskPriority? = nil,
+        _ updateAccumulatingResult: @escaping @Sendable (inout Result, Element) async throws -> ()
+    ) async rethrows -> Result {
+        let array = Array(self)
+        let chunks = stride(from: 0, to: array.count, by: chunkSize).map {
+            Array(array[$0..<Swift.min($0 + chunkSize, array.count)])
+        }
+        
+        return try await withThrowingTaskGroup(of: Result.self) { group in
+            var result = initialResult
+            
+            for chunk in chunks {
+                let currentResult = result
+                group.addTask(priority: priority) { @Sendable in
+                    var chunkResult = currentResult
+                    for element in chunk {
+                        try await updateAccumulatingResult(&chunkResult, element)
+                    }
+                    return chunkResult
+                }
+            }
+            
+            while let chunkResult = try await group.next() {
+                result = chunkResult
+            }
+            
+            return result
+        }
+    }
+} 
