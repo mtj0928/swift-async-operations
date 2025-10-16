dd53e7f1b7d48a45d3792c90390fb2309e7dc0f8
mtj0928 <mtj0928@users.noreply.github.com>
2025-10-16 10:37:29 +0900

    Make unit tests stable

--- Tests/AsyncOperationsTests/Sequence/SequenceAsyncForEachTests.swift
+++ Tests/AsyncOperationsTests/Sequence/SequenceAsyncForEachTests.swift
@@ -17,26 +17,26 @@ final class SequenceAsyncForEachTests: XCTestCase {
         var events: [ConcurrentTaskEvent] = []
         let numberOfElements = 10
         let numberOfConcurrentTasks = 3
-        // A random offset to stagger iteration timings.
+        // An offset to stagger iteration timings.
         let randomOffsets: [Double] = [
-            470,
-            150,
-            420,
-            290,
-            670,
-            810,
-            930,
-            120,
-            540,
-            760,
+            2,
+            1,
+            0,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
         ]
         try await (0..<numberOfElements).asyncForEach(numberOfConcurrentTasks: UInt(numberOfConcurrentTasks)) { @MainActor number in
             events.append(.start)
             let offsets = randomOffsets[number]
-            try await Task.sleep(for: .milliseconds(100 * Double(numberOfElements - number) + offsets))
+            try await Task.sleep(for: .milliseconds(10 * offsets))
             events.append(.end)
         }
-        print(events.map(\.rawValue))
         XCTAssertEqual(Array(events.prefix(numberOfConcurrentTasks)), Array(repeating: .start, count: numberOfConcurrentTasks))
         XCTAssertEqual(
             events[numberOfConcurrentTasks..<(2 * numberOfElements - numberOfConcurrentTasks)].map { $0 },
